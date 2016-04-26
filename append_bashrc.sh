db8a692d29abddf357594ca04858db08a3bb0f2c
Jeanderson Candido <jeandersonbc@gmail.com>
2016-04-25 22:22:12 -0300

    Added file extensions

--- append_bashrc.sh
+++ append_bashrc.sh
@@ -2,7 +2,7 @@
 existing_line=`cat $HOME/.bashrc | grep my_profile`
 if [ -z "$existing_line" ]; then
     path=`pwd`
-    echo ". $path/my_profile" >> $HOME/.bashrc
+    echo ". $path/my_profile.sh" >> $HOME/.bashrc
 else
     echo "Reference already exists on .bashrc"
 fi
